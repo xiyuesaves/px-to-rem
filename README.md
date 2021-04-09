2e3d44cdb4ddd0a09d93cedfd28e3c50cb18cf4a
xiyue <xiyuesaves@gmail.com>
2021-04-09 11:18:16 +0800

    first upload

---
+++ README.md
@@ -0,0 +1,46 @@
+rem-unit-fix
+-------------
+一个输入px可转为rem的Sublime Text 3自动完成插件。
+
+#### 与rem-unit的区别
+
+* 修复负数无法转换的bug
+
+* 增加选项是否保留px单位
+
+* 新增首行激活字符串
+
+  > (当文件首行含有指定字符串时,才激活转换功能,默认为空)
+
+  ![首行字符串激活](firstline.gif)
+
+##### 与CSSREM的区别
+* 删除多余的小数 1.0rem -> 1rem
+* 删除0px转换时多余的单位 0.0rem -> 0
+* 增加 前导0的选择 0.25rem -> .25rem
+* 增加 可在注释保留原始值 css使用块注释 其他语言行注释
+* 增加默认支持文件类型.scss .styl
+* TODO: 小数px起始替换位置不正确的bug 16.16px -> 16.1.01rem (should be 1.01rem)
+
+##### 插件效果：（CSSREM）
+![效果演示图](cssrem.gif)
+
+#### 安装(Package Control)
+* Ctrl+Shift+P 输入 Package Control: Install Packag
+* 搜索 rem-unit-fix
+* 重启 Sublime Text
+
+##### 配置参数
+
+参数配置文件：Sublime Text -> Preferences -> Package Settings -> rem-unit-fix
+
+* `fontsize` - html元素font-size值，默认为16。
+* `precision` - px转rem的小数部分的最大长度，默认为8。
+* `leadingzero` - 是否保留前导0，默认不保留。
+* `exts` - 启用此插件的文件类型。默认为：[".css", ".scss", ".less", ".sass", ".styl"]。
+* `firstline` - 指定激活字符串
+* `reservedunit` - 是否保留px单位
+
+#### 感谢
+* rem-unit 作者  https://github.com/fisker/rem-unit
+* CSSREM 作者 https://github.com/flashlizi/cssrem
---
+++ rem-unit-fix.py
@@ -0,0 +1,130 @@
+
+import sublime
+import sublime_plugin
+import re
+import time
+import os
+
+SETTINGS = {}
+lastCompletion = {"needFix": False, "value": None, "region": None}
+
+    # 插件加载完成
+def plugin_loaded():
+    # 调用初始化设置
+    init_settings()
+
+    # 初始化设定
+def init_settings():
+    # 从配置文件获取设置
+    get_settings()
+    # 应该是动态监听配置文件变动的
+    sublime.load_settings('rem-unit.sublime-settings').add_on_change('get_settings', get_settings)
+
+    # 从配置文件加载设置
+def get_settings():
+    settings = sublime.load_settings('rem-unit.sublime-settings')
+    SETTINGS['fontsize'] = settings.get('fontsize', 16)
+    SETTINGS['precision'] = settings.get('precision', 8)
+    SETTINGS['exts'] = settings.get('exts', [".css", ".scss", ".less", ".sass", ".styl"])
+    SETTINGS['leadingzero'] = settings.get('leadingzero', False)
+    SETTINGS['firstline'] = settings.get('firstline', '')
+    SETTINGS['reservedunit'] = settings.get('reservedunit', False)
+
+    # 获取设定中某一项值
+def get_setting(view, key):
+    return view.settings().get(key, SETTINGS[key]);
+
+    # 插件的事件监听
+class CssRemCommand(sublime_plugin.EventListener):
+    def on_text_command(self, view, name, args):
+        if name == 'commit_completion':
+            view.run_command('replace_rem')
+        return None
+
+    def on_query_completions(self, view, prefix, locations):
+        # print('rem-unit start {0}, {1}'.format(prefix, locations))
+
+        # 判断是否符合文件类型不符合直接return出去
+        fileName, fileExtension = os.path.splitext(view.file_name())
+        # 添加判断首行是否含有特定字符串
+        firstLine = view.substr(view.line(0))
+        match = re.search("(" + get_setting(view, 'firstline') + ")+", firstLine)
+        # print(firstLine, match, match == None)
+        if not fileExtension.lower() in get_setting(view, 'exts') or match == None:
+            return []
+
+        # 重置完成对比
+        lastCompletion["needFix"] = False
+        location = locations[0]
+        snippets = []
+
+        # 计算出rem值
+        match = re.compile("(-?[\d.]+)p(x)?").match(prefix)
+        if match:
+            lineLocation = view.line(location)
+            line = view.substr(sublime.Region(lineLocation.a, location))
+            value = match.group(1)
+
+            # 修复值类似"0.5px"
+            segmentStart = line.rfind(" ", 0, location)
+            if segmentStart == -1:
+                segmentStart = 0
+            segmentStr = line[segmentStart:location]
+
+            segment = re.compile("(-?[\d.])+" + value).search(segmentStr)
+            if segment:
+                value = segment.group(0)
+                start = lineLocation.a + segmentStart + 0 + segment.start(0)
+                lastCompletion["needFix"] = True
+            else:
+                start = location
+
+            remValue = round(float(value) / get_setting(view, 'fontsize'), get_setting(view, 'precision'))
+
+            # 删除无用的".0"
+            intValue = int(remValue)
+            if intValue == remValue:
+                remValue = intValue
+
+            strRem = str(remValue)
+
+            # 删除前导零
+            if (get_setting(view, 'leadingzero') == False) and (remValue < 1.0):
+                strRem = strRem[1:]
+
+            # 如果值是0则删除单位
+            if remValue != 0:
+                strRem += 'rem'
+            else:
+                strRem = '0'
+
+            # 保存以进行替换
+            lastCompletion["value"] = strRem
+            lastCompletion["region"] = sublime.Region(start, location)
+
+            commentStr = '';
+            if (fileExtension.lower() in [".sass", ".scss", ".styl", ".less"]):
+                commentStr = '; // ' + value + 'px';
+            else:
+                commentStr = '/* ' + value + 'px */';
+
+            if re.compile("(-){1}").match(prefix):
+                strRem = '-' + strRem
+
+            # set completion snippet
+            if get_setting(view, 'reservedunit'):
+                snippets += [(value + 'px -> ' + strRem + '(keep px value)', strRem + commentStr)]
+            else:
+                snippets += [(value + 'px -> ' + strRem + '(' + str(get_setting(view, 'fontsize')) + 'px/rem)', strRem)]
+        # print("rem-unit: {0}".format(snippets))
+        return snippets
+
+class ReplaceRemCommand(sublime_plugin.TextCommand):
+    def run(self, view, args):
+        needFix = lastCompletion["needFix"]
+        if needFix == True:
+            value = lastCompletion["value"]
+            region = lastCompletion["region"]
+            # print('replace: {0}, {1}'.format(value, region))
+            view.replace(region, value)
+            view.end_edit()
